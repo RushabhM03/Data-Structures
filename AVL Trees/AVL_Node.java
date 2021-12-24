8d926eafedded6a58d8fe37bd95d67537959c20d
Rushabh Maru <68846562+RushabhM03@users.noreply.github.com>
2021-12-24 14:43:52 +0530

    Add files via upload

---
+++ AVL Trees/AVL_Node.java
@@ -0,0 +1,16 @@
+package DSA;
+
+
+/**
+ * Write a description of class AVL_Node here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AVL_Node
+{
+    AVL_Node left;
+    AVL_Node right;
+    int data;
+    int height; // height of node
+}
