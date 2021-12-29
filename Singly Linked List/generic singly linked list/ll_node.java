a6fdc9df014b9a1516f780539aa8ece20723f129
Rushabh Maru <68846562+RushabhM03@users.noreply.github.com>
2021-12-29 22:48:16 +0530

    Add files via upload

---
+++ Singly Linked List/generic singly linked list/ll_node.java
@@ -0,0 +1,14 @@
+package DSA;
+
+
+/**
+ * Write a description of class ll_node here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ll_node <T>
+{
+    T data;
+    ll_node next;
+}
